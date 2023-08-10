0364ae8612eb86d6c9e848cf80b654bbc7915529
AlexStorm <info@alexstorm.ru>
2023-08-10 15:13:18 +0530

    update init interface

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import {RemoteEntryModule} from "./app/entry.module";
 export * from "./app/entry.module";
 import {XsModule, XsModuleLayout} from "@solenopsys/fl-globals";
 
-export const ENTRY: XsModule<RemoteEntryModule> = new XsModuleLayout<RemoteEntryModule>();
+export const ENTRY: XsModule<RemoteEntryModule> = new XsModuleLayout<RemoteEntryModule>(RemoteEntryModule);
 
